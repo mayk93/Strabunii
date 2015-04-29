a85cc0fe2d61e4ec39c17715de20460f4a73b1d5
mayk93 <mihai.mandrescu@gmail.com>
2015-04-29 21:03:10 +0300

    Improved Mouse Over
    - Implemented clear tile selection using a selector prefab.

--- Strabunii/Assets/Scripts/Map/TileMapMouse.cs
+++ Strabunii/Assets/Scripts/Map/TileMapMouse.cs
@@ -2,29 +2,51 @@
 using System.Collections;
 
 /* This script will handle mouseing over the map */
+[RequireComponent(typeof(TileMap))]
 public class TileMapMouse : MonoBehaviour 
 {
 	private Collider collider;
 	private Renderer renderer;
+	private TileMap tileMap;
+	private Vector3 currentTileCoordinate;
+
+	public GameObject Selection;
 
 	void Start() 
 	{
+		tileMap = GetComponent<TileMap> ();
 		collider = GetComponent<Collider>();
 		renderer = GetComponent<Renderer> ();
 	}
 	// Update is called once per frame
 	void Update () 
 	{
-		Ray ray = Camera.mainCamera.ScreenPointToRay (Input.mousePosition);
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hitInfo;
 
 		if (collider.Raycast (ray, out hitInfo, Mathf.Infinity)) 
 		{
-			renderer.material.color = Color.red;
+			/* 
+			 * Here, x and z refer to the position of the mouse on the map.
+			 * We substract the maps position to ensure everything is relative to it.
+			 * We devide by the tileSize in order to account for the size of the tiles.
+			 */
+			int x = Mathf.FloorToInt(hitInfo.point.x - transform.position.x / tileMap.tileSize); 
+			int z = Mathf.FloorToInt(hitInfo.point.z - transform.position.z / tileMap.tileSize);
+
+			currentTileCoordinate.x = x;
+			currentTileCoordinate.z = z;
+
+			Selection.transform.position = currentTileCoordinate*tileMap.tileSize;
 		} 
 		else 
 		{
-			renderer.material.color = Color.green;
+			/* Here we hide the selection */
+		}
+
+		if( Input.GetMouseButton(0) )
+		{
+			print ("Move unit here.");
 		}
 	}
 }
